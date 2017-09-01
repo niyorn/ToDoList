9907b72233d35dd5ecc3ae9f7a286c060e6ecc1d
unknown <c.niyornram@hotmail.com>
2017-09-02 01:41:19 +0200

    'lol'

--- script/script.js
+++ script/script.js
@@ -1,24 +1,26 @@
 //---------------------------------------------------Variable------------------------------------------------
 var voegTaakTekst = document.querySelector('header input');
 var voegTaakButton = document.querySelector('header button');
-
+var storage = window.localStorage;
+var index;
 
 //----------------------------------------------------Function-----------------------------------------------
 //Kijkt of de gebruiker een taak ingegevuld heeft
 function voegTaak() {
     var inputTaak = document.querySelector('header input').value;
-
     if (inputTaak) {
-        voegTaakAanLijst(inputTaak); //voeg taak aan lijst
+        //voeg taak toe aan localhost
+        voegTaakAanLijst(inputTaak, storeData(inputTaak)); //voeg taak aan lijst
         resetTekst();
     }
 }
 
 //Voegt taak aan lijst toe
-function voegTaakAanLijst(inputTaak) {
+function voegTaakAanLijst(inputTaak, index) {
     var lijst = document.querySelector('UL');
     var taak = document.createElement('LI');
     taak.innerText = inputTaak; //voeg de tekst aan toe LI element
+    taak.setAttribute("id", index);
     var verwijderButton = document.createElement('BUTTON');
     var verwijderIcoon = document.createElement('I');
     verwijderIcoon.classList.add('fa', 'fa-trash-o', 'fa-2x'); //de icoon wordt geladen via http://fontawesome.io/ daarom de classes
@@ -32,29 +34,41 @@ function voegTaakAanLijst(inputTaak) {
 
 //voeg taak aan lijst als de gebruiker op enter drukt
 function voegTaakAanLijstEnterPress(e){
-  var taak = this.value;
-  
-  //Als er niks ingevuld word, geef dan niks door
-  if(taak.length == 0){
-    return false;
-  }
-
   if(e.key == 'Enter'){
-    voegTaakAanLijst(taak);
+    voegTaak();
     resetTekst();
   }
 }
 
-function verwijderTaak() {
-  console.log(this.parentNode.parentNode);
+function verwijderTaak(e) {
+    var listID = this.parentNode.id;
     var lijst = this.parentNode.parentNode; // de lijst
     var item = this.parentNode; // de item
     item.classList.add('verwijder');
 
     //verwijder de item pas als de css animatie afgespeeld is
     setTimeout(function() {
+      storage.removeItem(listID)
       lijst.removeChild(item); //verwijder de item
     }, 400);
+}
+
+
+//hier
+//slaat de todo op in de localstorage
+function storeData(value){
+  let time = Date.now();
+    storage.setItem(time, value);
+    return time;
+}
+
+//laad bestaande todo lijst
+function loadStorage() {
+    for (let i = 0; i < storage.length; i++){
+      let indexItem = storage.key(i);
+      let item = localStorage.getItem(indexItem);
+        voegTaakAanLijst(item, indexItem);
+  };
 
 }
 
@@ -63,9 +77,11 @@ function resetTekst(){
 }
 
 
-
 //-------------------------------------------------------Eventlistener-------------------------------------
 //Gebruiker klikt op toevoegen
 voegTaakButton.addEventListener('click', voegTaak);
 //Gebruiker klikt op Enter
 voegTaakTekst.addEventListener('keypress', voegTaakAanLijstEnterPress);
+
+//Zorg voor het cachen van de TODO items
+window.addEventListener('DOMContentLoaded', loadStorage);
